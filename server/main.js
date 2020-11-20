3bdc6ece1dde4f9399af262bee8b9dd52c519291
JP Lomas <jp@theqrl.org>
2020-11-20 11:15:26 +0000

    Revert CORS

--- server/main.js
+++ server/main.js
@@ -7,15 +7,6 @@ const testnet = new QrlNode(ip, port)
 
 Meteor.startup(() => {
 
-  // Enable cross origin requests for all endpoints
-  JsonRoutes.setResponseHeaders({
-    'Cache-Control': 'no-store',
-    Pragma: 'no-cache',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
-  });
-
   testnet.connect().then(() => {
     console.log('Connected to Testnet')
 
